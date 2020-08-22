181b650c2eff2c4c6fff9c57c773b8925fd1e364
achintyatripathi <35167273+achintyatripathi@users.noreply.github.com>
2020-08-22 23:46:06 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,24 @@ const instance = new Typewriter('#typewriter', {
     loop: true
 });
 
+
+instance.typeString("Hello World!")
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('I am Satvik Chachra.')
+    .pauseFor(1000)
+    .deleteChars(15)
+    .typeString('an aspiring<br>Software Engineer.')
+    .pauseFor(1000)
+    .deleteChars(29)
+    .typeString('currently learning<br>Web Development.')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('Check out my work at<br>github.com/satvikchachra')
+    .pauseFor(1000)
+    .deleteAll()
+    .start();
+<!--
 instance.typeString("Hi Everyone!")
     .pauseFor(1000)
     .deleteAll()
@@ -26,4 +44,5 @@ instance.typeString("Hi Everyone!")
     .pauseFor(1000)
     .deleteAll()
     .start();
+-->
 © 2020 GitHub, Inc.
