ecc3c77d17d3d18815dd73a457702fa0f2bbe310
achintyatripathi <35167273+achintyatripathi@users.noreply.github.com>
2020-08-22 23:46:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,27 +22,5 @@ instance.typeString("Hello World!")
     .pauseFor(1000)
     .deleteAll()
     .start();
-<!--
-instance.typeString("Hi Everyone!")
-    .pauseFor(1000)
-    .deleteAll()
-    .typeString('I am Achintya Tripathi.')
-    .pauseFor(1000)
-    .deleteChars(23)
-    .typeString('an aspiring<br>ML / Deep Learning enthusiast.')
-    .pauseFor(1000)
-    .deleteChars(42)
-    .typeString('currently learning<br>Statistic & ML Algorithms.')
-    .pauseFor(1000)
-    .deleteAll()
-    .typeString('Check out my work at<br>')
-    .pauseFor(1000)
-    .typeString('github.com/achintyatripathi')
-    .pauseFor(1000)
-    .deleteChars(42)
-    .typeString('https://www.kaggle.com/achintyatripathi')
-    .pauseFor(1000)
-    .deleteAll()
-    .start();
--->
+
 © 2020 GitHub, Inc.
